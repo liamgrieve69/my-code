b0007a1e3dda94f4de66212f2f33ccc2f9d7dd0f
Liam Grieve <lgrieve17@gmail.com>
2022-02-16 17:01:10 +0000

    third lesson of python
    third lesson of python from networkchuck

---
+++ python/learn python/lrn3.py
@@ -0,0 +1,18 @@
+name = "liam"
+# varible
+
+age = 15
+# varible interger
+
+actual_age = 15.4
+# floating point number
+
+print(name)
+print(age)
+# printing them out
+
+print(type(name))
+print(type(age))
+print(type(actual_age))
+# tells us what type the varible is
+
