fe15092442440191cacafd4e5c3076726daf925c
Liam Grieve <lgrieve17@gmail.com>
2022-02-16 17:00:44 +0000

    second lesson of python
    second lesson of python from networkchuck

---
+++ python/learn python/lrn2.py
@@ -0,0 +1,19 @@
+#build a robot
+
+print("hello, welcome to networkchuck coffee!!!!")
+#saying a frase
+name = input("what is your name?\n")
+#create varable after asking quiestions and new line for the variable text
+
+print("Hello " + name + ", thank you so much for coming in today\n\n\n")
+#print string plus varible plus 2nd string plus some spaceing
+
+menu = "Black coffee, espresso, latte, cappucino"
+#make a manu in a varible
+
+print(name + ", what would you like from our menu today? Here is what we are serving.\n" + menu)
+# ask what you weould like as a string and say the varible menu
+
+order = input()
+
+print("sound good "+ name + ", we'll have that " + order + " ready for you in a moment")
