f87f69f456d3a24872135560ccdc26f1d2083aab
Liam Grieve <lgrieve17@gmail.com>
2022-02-16 17:00:18 +0000

    first lesson of python
    first lesson of python from networkchuck

---
+++ python/learn python/lrn1.py
@@ -0,0 +1,16 @@
+print("hello world!!!! ")
+print(" i am iron man ")
+#basic print 2 diffrent string and diffrent lines
+
+print("hello world!!!. i am iron man.")
+#multi same line
+
+print("""hello world.
+i am iron man""")
+#multi diffrent lines the 3 "" means more string on diffrent lines
+
+print("hello world\n"+"i am iron man")
+#same diffrent lines the \n means new line the + means add more
+
+print("i am iron man\n" * 100)
+#1 string diffren lines times 100
